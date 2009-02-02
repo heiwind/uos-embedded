cddec0105c41261b698cdd12fc000744970e2d19
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2009-02-02 18:07:45 +0000

    Small changes in examples/avr-atmega168.

--- examples/avr-atmega168/test_debug.c
+++ examples/avr-atmega168/test_debug.c
@@ -6,7 +6,7 @@
 int main (void)
 {
 	/* Baud 19200. */
-	outb (((int) (KHZ * 1000L / 19200) + 8) / 16 - 1, UBRR);
+	outw (((int) (KHZ * 1000L / 19200) + 8) / 16 - 1, UBRR);
 
 	for (;;) {
 		debug_puts ("Hello, World!\n");
