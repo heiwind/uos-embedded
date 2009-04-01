708b5e07f01ebe39fe001060c13fb33ac4f5ef5d
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2009-04-01 16:42:41 +0000

    SAM7: implemented ADC. Started test for Nokia 6100 LCD display.

--- sources/adc/adc.c
+++ sources/adc/adc.c
@@ -9,13 +9,14 @@
 /*
  * Read a result from ADC.
  */
-unsigned short
+unsigned
 adc_read (adc_t *v)
 {
-	unsigned short val;
+	unsigned val;
 
 	lock_take (&v->lock);
 
+#if __AVR__
 	/* Start measuring in Single Conversion mode. */
 	setb (ADSC, ADCSR);
 
@@ -26,7 +27,18 @@ adc_read (adc_t *v)
 
 	val = ADCL;
 	val |= ADCH << 8;
+#endif
 
+#if ARM_AT91SAM
+	/* Start conversion. */
+	*AT91C_ADC_CR = AT91C_ADC_START;
+
+	/* Wait for end of convertion. */
+	while (! (*AT91C_ADC_SR & (1 << v->channel)))
+		continue;
+
+	val = AT91C_ADC_CDR0 [v->channel];
+#endif
 	lock_release (&v->lock);
 	return val;
 }
@@ -35,18 +47,39 @@ adc_read (adc_t *v)
  * Select analog channel connected to ADC.
  */
 void
-adc_select_channel (adc_t *v, unsigned char cnum)
+adc_select_channel (adc_t *v, small_uint_t cnum)
 {
-	ADMUX = cnum;
+	v->channel = cnum;
+#if __AVR__
+	ADMUX = v->channel;
+#endif
+#if ARM_AT91SAM
+	/* Enable desired channel */
+	*AT91C_ADC_CHER = 1 << v->channel;
+#endif
 }
 
 void
 adc_init (adc_t *v)
 {
+#if __AVR__
 	/* Turn ADC on, prescaler 1/128. */
 	ADCSR = 0x87;
 
 	/* Get the interrupt. */
 	lock_take_irq (&v->lock, ADC_IRQ, 0, 0);
 	lock_release (&v->lock);
+#endif
+
+#if ARM_AT91SAM
+	/* Enable clock for interface. */
+	*AT91C_PMC_PCER = 1 << AT91C_ID_ADC;
+
+	/* Reset */
+	*AT91C_ADC_CR = 0x1;
+	*AT91C_ADC_CR = 0x0;
+
+	/* Set maximum startup time and hold time. */
+	*AT91C_ADC_MR = 0x0F1F0F00;
+#endif
 }
--- sources/adc/adc.h
+++ sources/adc/adc.h
@@ -1,7 +1,8 @@
 typedef struct {
 	lock_t lock;
+	small_uint_t channel;
 } adc_t;
 
 void adc_init (adc_t *v);
-void adc_select_channel (adc_t *v, unsigned char cnum);
-unsigned short adc_read (adc_t *u);
+void adc_select_channel (adc_t *v, small_uint_t cnum);
+unsigned adc_read (adc_t *u);
