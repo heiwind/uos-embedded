4c1220ebc44aa24388d8ba583e54196ca8008e5d
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2009-04-21 16:27:02 +0000

    SAM9 FPGA cleanup.

--- examples/arm-sam9-s3e/fpga/sram_to_arm.v
+++ examples/arm-sam9-s3e/fpga/sram_to_arm.v
@@ -5,25 +5,25 @@
 
 module sram_to_arm (
 		inout [15:0] ARM_D,
-		input [18:1] ARM_A,
-		input ARM_NLB,
-		input ARM_CS,
-		input ARM_OE,
-		input ARM_WE,
+		input [18:0] ARM_A,
+		input ARM_nLB,
+		input ARM_nCS,
+		input ARM_nOE,
+		input ARM_nWE,
 
 		inout [7:0] SRAM_D,
 		output [18:0] SRAM_A,
-		output SRAM_CS,
-		output SRAM_OE,
-		output SRAM_WE
+		output SRAM_nCS,
+		output SRAM_nOE,
+		output SRAM_nWE
 	);
 
-	assign SRAM_CS = ARM_CS;
-	assign SRAM_OE = ARM_OE;
-	assign SRAM_WE = ARM_WE;
-	assign SRAM_A  = { ARM_A, ARM_NLB };
+	assign SRAM_nCS = ARM_nCS;
+	assign SRAM_nOE = ARM_nOE;
+	assign SRAM_nWE = ARM_nWE;
+	assign SRAM_A   = ARM_A;
 
-	assign ARM_D[15:0] = (!ARM_OE && !ARM_CS) ? { 8'h00, SRAM_D[7:0] } : 16'hzzzz;
-	assign SRAM_D[7:0] = (!ARM_WE && !ARM_CS) ? ARM_D[7:0] : 8'hzz;
+	assign ARM_D[15:0] = (!ARM_nOE && !ARM_nCS) ? { 8'h00, SRAM_D[7:0] } : 16'hzzzz;
+	assign SRAM_D[7:0] = (!ARM_nWE && !ARM_nCS) ? ARM_D[7:0] : 8'hzz;
 
 endmodule
