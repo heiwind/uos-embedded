918f4a7145ec83468ed2c86aafde224faa08fb39
vatilin <vatilin@319a1301-964c-0410-93c1-575b8fb0c566>
2010-05-06 15:11:44 +0000

    MIPS32: Функция задержки (udelay) специализирована для процессоров серии MC-24 (используется счетчик тактов)

--- sources/runtime/mips32/udelay.c
+++ sources/runtime/mips32/udelay.c
@@ -2,8 +2,28 @@
 
 /*
  * Microsecond delay.
- * TODO: calibrate for Elvees MC-24!
  */
+
+#ifdef ELVEES_MC24
+
+/* Specialization for Elvees MC-24 */
+
+#include <runtime/mips32/io.h>
+
+#define TIME_BEFORE(unknown, known) ((long)(unknown) - (long)(known) < 0)
+
+void udelay (unsigned usec)
+{
+	unsigned timeout = mips32_read_c0_register(C0_COUNT) + usec * (KHZ / 1000);
+	while ( TIME_BEFORE( mips32_read_c0_register(C0_COUNT), timeout ) ) {
+		asm volatile ("nop");
+	};
+}
+
+#else
+
+/* Generic delay function (has to be calibrated for each CPU type) */
+
 void udelay (unsigned usec)
 {
 	unsigned nloops;
@@ -23,3 +43,5 @@ void udelay (unsigned usec)
 		asm volatile ("nop");
 	} while (--nloops);
 }
+
+#endif
