6b0f2962f2bb5f8996627c6db452485ec7d603a0
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2010-09-03 16:36:52 +0000

    Cortex-M3: сделаты тесты латентности прерываний, времени входа в прерывание, пять обедающих философов.

---
+++ examples/arm-1986be91/test-latency.c
@@ -0,0 +1,97 @@
+/*
+ * Измерение времени входа в прерывание.
+ */
+#include <runtime/lib.h>
+#include <kernel/uos.h>
+#include <stream/stream.h>
+#include <gpanel/gpanel.h>
+#include <timer/timer.h>
+
+#define TIMER_IRQ	32	/* Прерывание Systick */
+#define MSEC_PER_TICK	100	/* Период таймера в миллисекундах */
+
+ARRAY (task, 1000);
+gpanel_t display;
+mutex_t timer_lock;
+
+volatile unsigned nirqs;
+volatile unsigned latency_min = ~0;
+volatile unsigned latency_max;
+
+/*
+ * Печать рационального числа a/b с двумя знаками после запятой.
+ */
+void print_rational (char *title, unsigned a, unsigned b)
+{
+	unsigned val = a * 100 / b;
+
+	printf (&display, "%s%u.%02u usec\n", title, val/100, val%100);
+}
+
+/*
+ * Задача выдачи статистики на консоль.
+ */
+void console (void *arg)
+{
+	for (;;) {
+		mutex_wait (&timer_lock);
+		gpanel_move (&display, 0, 0);
+		printf (&display, "CPU: %u MHz\n\n", KHZ / 1000);
+
+		printf (&display, "Interrupts: %u\n\n", nirqs);
+
+		if (nirqs > 10) {
+			printf (&display, "IRQ latency:\n");
+			print_rational ("  min: ", latency_min * 1000, KHZ);
+			print_rational ("  max: ", latency_max * 1000, KHZ);
+		}
+	}
+}
+
+/*
+ * Быстрый обработчик прерывания от интервального таймера.
+ * Аргумент вызова не используется.
+ */
+static bool_t timer_handler (void *arg)
+{
+	/* Вычисляем количество тактов, затраченных на вход в прерывание. */
+	unsigned latency = ARM_SYSTICK->LOAD - ARM_SYSTICK->VAL;
+
+	/*debug_printf ("<%u> ", latency);*/
+	if (++nirqs > 10) {
+		if (latency_min > latency)
+			latency_min = latency;
+		if (latency_max < latency)
+			latency_max = latency;
+	}
+
+	/* Возвращаем 0, чтобы послать сигнал для mutex_wait(). */
+	return 0;
+}
+
+void uos_init (void)
+{
+	extern gpanel_font_t font_fixed6x8;
+
+	/* Стираем экран. */
+	gpanel_init (&display, &font_fixed6x8);
+	gpanel_clear (&display, 0);
+
+	/* Устанавливаем быстрый обработчик прерывания. */
+	mutex_attach_irq (&timer_lock, TIMER_IRQ, timer_handler, 0);
+
+	/* Используем интервальный таймер. */
+        ARM_SYSTICK->CTRL = 0;
+        ARM_SYSTICK->VAL = 0;
+#ifdef SETUP_HCLK_HSI
+        ARM_SYSTICK->LOAD = 8000 * MSEC_PER_TICK - 1;
+#else
+        ARM_SYSTICK->LOAD = KHZ * MSEC_PER_TICK - 1;
+#endif
+        ARM_SYSTICK->CTRL = ARM_SYSTICK_CTRL_ENABLE |
+                            ARM_SYSTICK_CTRL_TICKINT |
+                            ARM_SYSTICK_CTRL_HCLK;
+
+	/* Запускаем задачу выдачи статистики на консоль. */
+	task_create (console, "task", "console", 1, task, sizeof (task));
+}
