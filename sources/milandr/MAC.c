0fdafa28f45906f47a8adcd2fcf2f8747b0682c3
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2010-10-05 15:21:52 +0000

    Добавлен пропущенная функция mips-dump-stack(). Обновлены тексты 5600ВГ1У.

--- sources/milandr/MAC.c
+++ sources/milandr/MAC.c
@@ -1,70 +1,41 @@
 //
 // Определения функций и внутренних переменных.
 //
-// Project: Ethernet MAC
-// Author: Stanislav V. Afanas'ev
-// Company: Milandr
-// File: MAC.c
-// Version: 1.2
-// Date: 22.11.07
+// Project:	Ethernet MAC
+// Author:	Stanislav V. Afanas'ev
+// Company:	Milandr
+// File:	MAC.c
+// Version:	1.2
+// Date:	22.11.07
 //
-
 #include "MAC.h"
-#define irq_INT0	0x10
-#define irq_INT1	0x11
-#define irq_INT2	0x12
-#define irq_INT3	0x13
 
-#pragma DATA_SECTION(MAC_RG, ".RG_sect")
+#define	irq_INT0	0x10
+#define	irq_INT1	0x11
+#define	irq_INT2	0x12
+#define	irq_INT3	0x13
 
-	t_MAC MAC_RG;
+t_PACK_state stat_TX_PKG;
+t_PACK_state stat_RX_PKG;
 
-#pragma DATA_SECTION(TX_Descriptors, ".XD_sect")
+unsigned int free_size_TX = BUFF_SIZE;
 
-	t_TX_Buffer_Descriptor TX_Descriptors [DSCR_CNT];
+static unsigned int *ptr_TX_BUFF;
+t_INT_SOURCE int_SRC;
 
-#pragma DATA_SECTION(RX_Descriptors, ".RD_sect")
-
-	t_RX_Buffer_Descriptor RX_Descriptors [DSCR_CNT];
-
-#pragma DATA_SECTION(TX_buff, ".XB_sect")
-
-	unsigned int TX_buff [BUFF_SIZE];
-
-#pragma DATA_SECTION(RX_buff, ".RB_sect")
-
-	unsigned int RX_buff [BUFF_SIZE];
-
-	t_PACK_state stat_TX_PKG;
-	t_PACK_state stat_RX_PKG;
-
-	unsigned int free_size_TX = BUFF_SIZE;
-
-	static unsigned int *ptr_TX_BUFF;
-	t_INT_SOURCE int_SRC;
-
-	unsigned int TMP;
-	unsigned int *dst;
-	unsigned int *src;
-	unsigned int err;
-	unsigned int err_count;
-
-interrupt void c_int16()
-{
-	int_SRC.all = MAC_RG.INT_SOURCE.all;
-//	MAC_RG.INT_SOURCE.all = int_SRC.all;
-}
+unsigned TMP;
+unsigned *dst;
+unsigned *src;
+unsigned err;
+unsigned err_count;
 
 void MAC_reset (void)
 {
-	unsigned int i;
-	MAC_RG.GCTRL.bit.GLBL_RST = 1;
-	asm ("	rpt #7 ");
-	asm ("	nop ");
+	MAC_RG->GCTRL.bit.GLBL_RST = 1;
 
-	RX_Descriptors[0].RX_ctrl.all = 0x0000;
-	TX_Descriptors[0].TX_ctrl.all = 0x0000;
-	ptr_TX_BUFF = (void*) dflt_addr_Tx_BF;
+	RX_Descriptors[0].RX_ctrl.all = 0;
+	TX_Descriptors[0].TX_ctrl.all = 0;
+	ptr_TX_BUFF = (void *) dflt_addr_Tx_BF;
 
 	MAC_MEM_clear();
 	MAC_MEM_test();
@@ -73,7 +44,7 @@ void MAC_reset (void)
 
 void MAC_MEM_clear (void)
 {
-	unsigned int i;
+	unsigned i;
 
 	for (i = 0; i < sizeof(TX_buff); i++)
 		TX_buff[i] = 0;
@@ -99,18 +70,15 @@ void MAC_MEM_test (void)
 		if (RX_buff[i] != 0xFFFF)
 			err_count = err + 1;
 
-	asm (" nop ");
 	MAC_MEM_clear();
-	asm (" nop ");
 
 	for (i = 0; i < sizeof(TX_buff); i++)
 		MAC_MEM_CELL_test (&TX_buff[i], i);
 	for (i = 0; i < sizeof(RX_buff); i++)
 		MAC_MEM_CELL_test (&RX_buff[i], i);
-	asm (" nop ");
 }
 
-void MAC_MEM_CELL_test (unsigned int *addr, unsigned int value)
+void MAC_MEM_CELL_test (unsigned *addr, unsigned value)
 {
 	*addr = value;
 	if (*addr ^ value)
@@ -131,48 +99,44 @@ void MAC_MEM_CELL_test (unsigned int *addr, unsigned int value)
 
 void MAC_init (void)
 {
-	MAC_RG.GCTRL.all = dflt_GCTRL;
-	MAC_RG.base_MAC_RG = dflt_addr_RG;
-	MAC_RG.base_MAC_RxBF = dflt_addr_Rx_BF;
-	MAC_RG.base_MAC_TxBF = dflt_addr_Tx_BF;
-	MAC_RG.base_MAC_RxBD = dflt_addr_Rx_BD;
-	MAC_RG.base_MAC_TxBD = dflt_addr_Tx_BD;
+	MAC_RG->GCTRL.all	= dflt_GCTRL;
+	MAC_RG->base_MAC_RG	= dflt_addr_RG;
+	MAC_RG->base_MAC_RxBF	= dflt_addr_Rx_BF;
+	MAC_RG->base_MAC_TxBF	= dflt_addr_Tx_BF;
+	MAC_RG->base_MAC_RxBD	= dflt_addr_Rx_BD;
+	MAC_RG->base_MAC_TxBD	= dflt_addr_Tx_BD;
 
-	MAC_RG.MAC_CTRL.all = dflt_MAC_CTRL;
+	MAC_RG->MAC_CTRL.all	= dflt_MAC_CTRL;
 //BIT_TX_RST | BIT_RX_RST | BIT_READ_CLR_STAT | BIT_BIG_ENDIAN
 //BIT_PRO_EN | BIT_BCA_EN | BIT_MCA_EN | BIT_CTRL_FRAME_EN | BIT_LONG_FRAME_EN | BIT_SHORT_FRAME_EN | BIT_ERR_FRAME_EN
 //BIT_HALFD_EN | BIT_BCKOF_DIS | BIT_LB_EN
 
-	MAC_RG.PACKETLEN.MIN_FRAME = dflt_MinFrame;
-	MAC_RG.PACKETLEN.MAX_FRAME = dflt_MaxFrame;
-	MAC_RG.COLLCONF.all = dflt_CollConfig;
-	MAC_RG.IPGT = dflt_IPGTx;
+	MAC_RG->PACKETLEN.MIN_FRAME	= dflt_MinFrame;
+	MAC_RG->PACKETLEN.MAX_FRAME	= dflt_MaxFrame;
+	MAC_RG->COLLCONF.all		= dflt_CollConfig;
+	MAC_RG->IPGT			= dflt_IPGTx;
 
-	MAC_RG.MAC_ADDR[0] = dflt_MAC_ADDR_H;
-	MAC_RG.MAC_ADDR[1] = dflt_MAC_ADDR_M;
-	MAC_RG.MAC_ADDR[2] = dflt_MAC_ADDR_T;
-	MAC_RG.INT_MASK.all = dflt_INT_MASK;
-	MAC_RG.HASH[0] = 0x0000;
-	MAC_RG.HASH[1] = 0x0000;
-	MAC_RG.HASH[2] = 0x0000;
-	MAC_RG.HASH[3] = 0x8000;
+	MAC_RG->MAC_ADDR[0]	= dflt_MAC_ADDR_H;
+	MAC_RG->MAC_ADDR[1]	= dflt_MAC_ADDR_M;
+	MAC_RG->MAC_ADDR[2]	= dflt_MAC_ADDR_T;
+	MAC_RG->INT_MASK.all	= dflt_INT_MASK;
+	MAC_RG->HASH[0]		= 0;
+	MAC_RG->HASH[1]		= 0;
+	MAC_RG->HASH[2]		= 0;
+	MAC_RG->HASH[3]		= 0x8000;
 
-	MAC_RG.PHY_CTRL.all = dflt_PHY_CTRL;
+	MAC_RG->PHY_CTRL.all	= dflt_PHY_CTRL;
 //BIT_PHY_RST | BIT_PHY_EXT_EN | BIT_PHY_ERLY_DV | BIT_PHY_HALFD | BIT_PHY_DLB | BIT_PHY_LB
-
-	MAC_RG.RXBF_HEAD = dflt_addr_Rx_BF + sizeof_RxBF - 1;
-	MAC_RG.RXBF_TAIL = dflt_addr_Rx_BF;
-
-	asm (" rpt #7 ");
-	asm (" nop ");
+	MAC_RG->RXBF_HEAD	= dflt_addr_Rx_BF + sizeof_RxBF - 1;
+	MAC_RG->RXBF_TAIL	= dflt_addr_Rx_BF;
 }
 
-int chk_Send_OK (unsigned int *Dscr_Num)
+int chk_Send_OK (unsigned *Dscr_Num)
 {
 	if (! TX_Descriptors[*Dscr_Num].TX_ctrl.bit.RDY) {
-		stat_TX_PKG.num = *Dscr_Num;
+		stat_TX_PKG.num	 = *Dscr_Num;
 		stat_TX_PKG.stat = TX_Descriptors[*Dscr_Num].TX_ctrl.all;
-		free_size_TX += TX_Descriptors[*Dscr_Num].len;
+		free_size_TX	+= TX_Descriptors[*Dscr_Num].len;
 		if (free_size_TX > BUFF_SIZE)
 			free_size_TX = BUFF_SIZE;
 		return 0;
@@ -180,17 +144,17 @@ int chk_Send_OK (unsigned int *Dscr_Num)
 		return -1;
 }
 
-int chk_Receive_Ready(unsigned int *Dscr_Num)
+int chk_Receive_Ready (unsigned *Dscr_Num)
 {
 	if (! RX_Descriptors[*Dscr_Num].RX_ctrl.bit.EMPTY) {
-		stat_RX_PKG.num = *Dscr_Num;
+		stat_RX_PKG.num	 = *Dscr_Num;
 		stat_RX_PKG.stat = RX_Descriptors[*Dscr_Num].RX_ctrl.all;
 		return 0;
 	} else
 		return -1;
 }
 
-int Send_ETH_Pack (unsigned int *Dscr_Num, t_ETH_Pack *PKG, unsigned int PARAM)
+int Send_ETH_Pack (unsigned *Dscr_Num, t_ETH_Pack *PKG, unsigned PARAM)
 {
 	int i;
 
@@ -236,7 +200,7 @@ int Send_ETH_Pack (unsigned int *Dscr_Num, t_ETH_Pack *PKG, unsigned int PARAM)
 	return 0;
 }
 
-int Receive_ETH_Pack (unsigned int *Dscr_Num, t_ETH_Pack *PKG, unsigned int PARAM)
+int Receive_ETH_Pack (unsigned *Dscr_Num, t_ETH_Pack *PKG, unsigned PARAM)
 {
 	int i;
 	unsigned len;
--- sources/milandr/MAC.h
+++ sources/milandr/MAC.h
@@ -2,12 +2,12 @@
 // Объявления внешних функций и переменных, а также
 // макроопределения констант и регистровой модели контроллера ЛВС.
 //
-// Project: Ethernet MAC
-// Author: Stanislav V. Afanas'ev
-// Company: Milandr
-// File: MAC.h
-// Version: 1.2
-// Date: 22.11.07
+// Project:	Ethernet MAC
+// Author:	Stanislav V. Afanas'ev
+// Company:	Milandr
+// File:	MAC.h
+// Version:	1.2
+// Date:	22.11.07
 //
 
 //----------------------------------------------
@@ -17,10 +17,25 @@
 #define __MAC__
 #include "MAC_types.h"
 
+#define BASE_MAC		0x60000000
+#define MAC_RG			((t_MAC*) BASE_MAC)
+
+#define BASE_TX_DESC		0x60001000
+#define TX_Descriptors		((t_TX_Buffer_Descriptor*) BASE_TX_DESC)
+
+#define BASE_RX_DESC		0x60002000
+#define RX_Descriptors		((t_RX_Buffer_Descriptor*) BASE_RX_DESC)
+
+#define BASE_TX_DATA		0x60003000
+#define TX_buff			((unsigned*) BASE_TX_DATA)
+
+#define BASE_RX_DATA		0x60004000
+#define RX_buff			((unsigned*) BASE_RX_DATA)
+
 #define DSCR_CNT		32		// 32 descriptors
 #define DSCR_SIZE		8		// 32 descriptors
-#define BUFF_SIZE		(4096/2)	// 4096 kb WORD accessed
-#define BUFF_DSCR_SIZE		(DSCR_CNT*8/2)	// 8-byte length descriptors WORD accessed
+#define BUFF_SIZE		(4096/2)	// 4096 kb (WORD accessed)
+#define BUFF_DSCR_SIZE		(DSCR_CNT*8/2)	// 8-byte length descriptors (WORD accessed)
 #define RG_SET_SIZE		32		// 32 2byte registers
 
 #define MAC_SPC_BASE		0xE000		// MC/MP Memory space size
@@ -39,10 +54,10 @@
 #define bit_RXF			15	// Индикатор успешного приема пакета
 #define bit_RXE			14	// Индикатор наличия ошибок при приеме пакета
 #define bit_RXC			13	// Индикатор приема пакета управления
-#define bit_RXBF_FULL		12	// Индикатор переполнения буфера ПРМ при приеме пакета
+#define bit_RXBF_FULL		12	// Индикатор переполнения буфера ПРМ при приема пакета
 #define bit_DSCR_nREADY		11	// (зарезервировано)
 #define bit_RXL			10	// Индикатор приема пакета длиной более MaxFrame
-#define bit_RXS			9	// Индикатор приема пакета длинойменее MinFrame
+#define bit_RXS			9	// Индикатор приема пакета длиной менее MinFrame
 #define bit_MISSED_PKG		8	// (зарезервировано)
 
 #define bit_TXF			7	// Индикатор успешной передачи пакета
@@ -50,7 +65,7 @@
 #define bit_TXC			5	// Индикатор передачи пакета управления
 
 #define bit_TXBF_EMPTY		4	// (зарезервировано)
-#define bit_TXBF_EXHAUST	3	// (зарезервировано)
+#define bit_TXBF_EXHAUST 	3	// (зарезервировано)
 #define bit_TX_BUSY		0	// Индикатор принятия и обслуживания пакета Pause
 
 #define BIT_RXF			(1 << bit_RXF)
@@ -73,10 +88,7 @@
 //
 #define bit_TX_RST		15	// Сброс ПРД МАС-уровня (активный уровень "1")
 #define bit_RX_RST		14	// Сброс ПРM МАС-уровня (активный уровень "1")
-#define bit_TX_DSCR_SCAN_EN	12	// Переключение ПРД в режим сканирования
-					// дескрипторов, когда переход к следующему
-					// осуществляется вне зависимости от
-					// готовности (активный уровень "1")
+#define bit_TX_DSCR_SCAN_EN	12	// Переключение ПРД в режим сканирования дескрипторов, когда переход к следующему осуществляется вне зависимости от готовности (активный уровень "1")
 #define bit_PAUSE_EN		11	// Разрешения обработки Pause Frame (активный уровень "1")
 #define bit_PRO_EN		10	// Включение приема всех пакетов независимо от их МАС-адреса (активный уровень "1")
 #define bit_BCA_EN		9	// Включение приема всех широковещательных пакетов (активный уровень "1")
@@ -84,7 +96,7 @@
 #define bit_CTRL_FRAME_EN	7	// Разрешение приема управляющих пакетов (активный уровень "1")
 #define bit_LONG_FRAME_EN	6	// Разрешение приема пакетов длиной более MaxFrame (активный уровень "1")
 #define bit_SHORT_FRAME_EN	5	// Разрешение приема пакетов длиной менее MinFrame (активный уровень "1")
-#define bit_ERR_FRAME_EN	4	// Разрешение приема пакетов с ошибками (активный уровень "1")
+#define bit_ERR_FRAME_EN	4	// Разрешени приема пакетов с ошибками (активный уровень "1")
 #define bit_BCKOF_DIS		3	// Отключение интервала ожидания перед повторением отправки пакета в случае коллизии (активный уровень "1")
 #define bit_HALFD_EN		2	// Переключение в режим полудуплексных приема-передачи (активный уровень "1")
 #define bit_BIG_ENDIAN		1	// Переключение в режим BIG ENDIAN формата данных (активный уровень "1")
@@ -106,15 +118,14 @@
 #define BIT_BIG_ENDIAN		(1 << bit_BIG_ENDIAN)
 #define BIT_LB_EN		(1 << bit_LB_EN)
 
-
 //----------------------------------------------
 // TX_DESCRIPTOR_CTRL
 //
 #define bit_TX_DSCR_RDY		15	// Индикатор состояния исходящего пакета (1-готов, 0-отправлен/не заполнен)
 #define bit_TX_DSCR_WRAP	14	// Индикатор последнего дескриптора в таблице (1-переход к дескриптору #0)
-#define bit_TX_DSCR_IRQ_EN	13	// Разрешение формирования прерываний по передаче (1-вкл., 0выкл.)
+#define bit_TX_DSCR_IRQ_EN	13	// Разрешение формирования прерываний по передаче (1-вкл., 0-выкл.)
 #define bit_TX_DSCR_PRE_DIS	10	// Отключение передачи преамбулы (1-передача преамбулы отключена, 0-стандартный пакет)
-#define bit_TX_DSCR_PAD_DIS	9	// Отключение дополнения пакетов длиной менее MinFrame до минимальной длины PAD-ами (1-дополнение не производится, 0-дополнение производится)
+#define bit_TX_DSCR_PAD_DIS	9	// Отключение дополнения пакетов длиной менее MinFrame до минимальной длины PAD-ами (1-дополнение не производится, дополнения производится)
 #define bit_TX_DSCR_CRC_DIS	8	// Отключение дополнения пакетов полем CRC32
 #define bit_TX_DSCR_RL		3	// Индикатор исчерпания разрешенного кол-ва повторения в случае неуспешной отправки исходящего пакета
 #define bit_TX_DSCR_LC		2	// Индикатор наличия Late Collision
@@ -137,21 +148,21 @@
 //----------------------------------------------
 // RX_DESCRIPTOR_CTRL
 //
-#define bit_RX_DSCR_RDY		15 // Индикатор состояния исходящего пакета (1-готов к приему пакета, 0-пакет принят/не готов)
-#define bit_RX_DSCR_WRAP	14 // Индикатор последнего дескриптора в таблице (1-переход к дескриптору #0)
-#define bit_RX_DSCR_IRQ_EN	13 // Разрешение формирования прерываний по передаче (1-вкл., 0выкл.)
-#define bit_RX_DSCR_nRDY	11 // (зарезервировано)
-#define bit_RX_DSCR_MCA		10 // Индикатор приема группового пакета с MAC-адресом соответствующего HASH-таблице
-#define bit_RX_DSCR_BCA		9 // Индикатор приема широковещательного пакета
-#define bit_RX_DSCR_UCA		8 // Индикатор приема индивидуального пакета с полным совпадением MAC-адреса
-#define bit_RX_DSCR_CF		7 // Индикатор приема пакета управления
-#define bit_RX_DSCR_LF		6 // Индикатор приема пакета длиной более MaxFrame
-#define bit_RX_DSCR_SF		5 // Индикатор приема пакета длиной менее MinFrame
-#define bit_RX_DSCR_EF		4 // Индикатор наличия ошибок при приеме пакета (сводный бит по ошибкам см. ниже)
-#define bit_RX_DSCR_CRC_ERR	3 // Индикатор наличия ошибки CRC при приеме пакета
-#define bit_RX_DSCR_SMB_ERR	2 // Индикатор наличия ошибки в данных при приеме пакета
-#define bit_RX_DSCR_LC		1 // Индикатор наличия Late Collision
-#define bit_RX_DSCR_OR		0 // Индикатор переполнения буфера ПРМ при приеме пакета
+#define bit_RX_DSCR_RDY		15	// Индикатор состояния исходящего пакета (1-готов к приему пакета, 0-пакет принят/не готов)
+#define bit_RX_DSCR_WRAP	14	// Индикатор последнего дескриптора в таблице (1-переход к дескриптору #0)
+#define bit_RX_DSCR_IRQ_EN	13	// Разрешение формирования прерываний по передаче (1-вкл., 0-выкл.)
+#define bit_RX_DSCR_nRDY	11	// (зарезервировано)
+#define bit_RX_DSCR_MCA		10	// Индикатор приема группового пакета с MAC-адресом соответствующего HASH-таблице
+#define bit_RX_DSCR_BCA		9	// Индикатор приема широковещательного пакета
+#define bit_RX_DSCR_UCA		8	// Индикатор приема индивидуального пакета с полным совпадением MAC-адреса
+#define bit_RX_DSCR_CF		7	// Индикатор приема пакета управления
+#define bit_RX_DSCR_LF		6	// Индикатор приема пакета длиной более MaxFrame
+#define bit_RX_DSCR_SF		5	// Индикатор приема пакета длиной менее MinFrame
+#define bit_RX_DSCR_EF		4	// Индикатор наличия ошибок при приеме пакета (сводный бит по ошибкам см. ниже)
+#define bit_RX_DSCR_CRC_ERR	3	// Индикатор наличия ошибки CRC при приеме пакета
+#define bit_RX_DSCR_SMB_ERR	2	// Индикатор наличия ошибки в данных при приеме пакета
+#define bit_RX_DSCR_LC		1	// Индикатор наличия Late Collision
+#define bit_RX_DSCR_OR		0	// Индикатор переполнения буфера ПРМ при приеме пакета
 
 #define BIT_RX_DSCR_RDY		(1 << bit_RX_DSCR_RDY)
 #define BIT_RX_DSCR_WRAP	(1 << bit_RX_DSCR_WRAP)
@@ -185,8 +196,8 @@
 #define bit_PHY_EXT_EN		14	// Переключение на работу с внешним кнтроллером PHY-уровня (активный уровень "1")
 #define bit_PHY_TXEN		13	// Разрешение работы ПРД встроенного контроллера PHY-уровня (активный уровень "1")
 #define bit_PHY_RXEN		12	// Разрешение работы ПРМ встроенного контроллера PHY-уровня (активный уровень "1")
-#define bit_PHY_BASE_2		5	// Переключение на работу с коаксиальным кабелем в режиме полудуплесных приема-передачи (1-подлючение по коаксиальному кабелю, 0-подключение по витой паре)
-#define bit_PHY_DIR		4	// Порядок передачи битов в полубайте (1-прямой, 0-инверсный)
+#define bit_PHY_BASE_2		5	// Переключение на работу с коаксиальным кабелем в режиме полудуплесных приема-передачи (1-подлючение по коаксиальному кабелю,0-подключение по витой паре)
+#define bit_PHY_DIR		4	// Порядок передачи битов в полубайте (1-прямой,0-инверсный)
 #define bit_PHY_EARLY_DV	3	// Включение формирвания сигнала RxDV одновременно с сигналом CRS (активный уровень "1")
 #define bit_PHY_HALFD		2	// Включение режима полудуплесных приема-передачи (активный уровень "1")
 #define bit_PHY_DLB		1	// Включение тестового замыкания ПРД на ПРМ на входе контроллера PHY-уровня (активный уровень "1")
@@ -246,12 +257,12 @@
 //
 // SPI CTRL
 //
-#define bit_SPI_RX_EDGE		10	// Активный фронт ПРМ контроллера последовательного порта (1-положительный, 0-отрицательный)
-#define bit_SPI_TX_EDGE		9	// Активный фронт ПРД контроллера последовательного порта (1-положительный, 0-отрицательный)
+#define bit_SPI_RX_EDGE		10	// Активный фронт ПРМ контроллера последовательного порта (1-положительный,0-отрицательный)
+#define bit_SPI_TX_EDGE		9	// Активный фронт ПРД контроллера последовательного порта (1-положительный,0-отрицательный)
 #define bit_SPI_DIR		8	// Порядок передачи бит (1-MSB,0-LSB)
-#define bit_SPI_FRAME_POL	7	// Активный уровень сигнала кадровой синхронизации последовательного порта (1-положительный, 0-отрицательный)
-#define bit_SPI_CLK_POL		6	// Полярность тактового сигнала последовательного порта (1-инверсная, 0-прямая)
-#define bit_SPI_CLK_PHASE	5	// Фаза тактового сигнала последовательного порта (1-инверсная, 0-прямая)
+#define bit_SPI_FRAME_POL	7	// Активный уровень сигнала кадровой синхронизации последовательного порта  (1-положительный,0-отрицательный)
+#define bit_SPI_CLK_POL		6	// Полярность тактового сигнала последовательного порта  (1-инверсная,0-прямая)
+#define bit_SPI_CLK_PHASE	5	// Фаза тактового сигнала последовательного порта  (1-инверсная,0-прямая)
 #define bit_SPI_MASTER		4	// (зарезервировано)
 
 #define BIT_SPI_RX_EDGE		(1 << bit_SPI_RX_EDGE)
@@ -311,10 +322,10 @@
 #define dflt_IPGTx		0x0060
 #define dflt_INT_MASK		0x0000	// All interrupts disabled
 
-#define dflt_HASH0		0x0000
+#define dflt_HASH0		0x0000	// HAH is empty
 #define dflt_HASH1		0x0000
 #define dflt_HASH2		0x0000
-#define dflt_HASH3		0x8000	// Hash is empty
+#define dflt_HASH3		0x8000
 
 #define dflt_PHY_LINK_PERIOD	(0x0B << 6)
 #define dflt_PHY_CTRL		(BIT_PHY_RST | BIT_PHY_TXEN | BIT_PHY_RXEN | \
@@ -351,11 +362,11 @@
 #define cmd_Write		0x03
 #define cmd_Read		0x04
 
-#define SPI_CMD_RESET		(cmd_RESET << 4)
+#define SPI_CMD_RESET		(cmd_RESET     << 4)
 #define SPI_CMD_SelectSPI	(cmd_SelectSPI << 4)
 #define SPI_CMD_SelectPPI	(cmd_SelectPPI << 4)
-#define SPI_CMD_WRITE		(cmd_Write << 4)
-#define SPI_CMD_READ		(cmd_Read << 4)
+#define SPI_CMD_WRITE		(cmd_Write     << 4)
+#define SPI_CMD_READ		(cmd_Read      << 4)
 
 //
 // SPI Errors
@@ -369,14 +380,14 @@
 //----------------------------------------------
 // FUNCTION DECLARATIONS
 //
-void MAC_reset (void);
-void MAC_MEM_clear (void);
-void MAC_MEM_test (void);
-void MAC_MEM_CELL_test (unsigned int *addr, unsigned int value);
-void MAC_init (void);
-int Send_ETH_Pack (unsigned int *Dscr_Num, t_ETH_Pack *PKG, unsigned int PARAM);
-int Receive_ETH_Pack (unsigned int *Dscr_Num, t_ETH_Pack *PKG, unsigned int PARAM);
-int chk_Send_OK (unsigned int *Dscr_Num);
-int chk_Receive_Ready (unsigned int *Dscr_Num);
+void	MAC_reset (void);
+void	MAC_MEM_clear (void);
+void	MAC_MEM_test (void);
+void	MAC_MEM_CELL_test (unsigned int *addr, unsigned int value);
+void	MAC_init (void);
+int	Send_ETH_Pack (unsigned int *Dscr_Num, t_ETH_Pack *PKG, unsigned int PARAM);
+int	Receive_ETH_Pack (unsigned int *Dscr_Num, t_ETH_Pack *PKG, unsigned int PARAM);
+int	chk_Send_OK (unsigned int *Dscr_Num);
+int	chk_Receive_Ready (unsigned int *Dscr_Num);
 
 #endif // __MAC__
