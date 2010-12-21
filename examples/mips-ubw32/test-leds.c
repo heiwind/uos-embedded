e056bc9c387a67cc02bd402eaa3ccb0ab1144e89
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2010-12-21 16:00:18 +0000

    UBW32: работает тест отладочной печати на UART.

--- examples/mips-ubw32/test-leds.c
+++ examples/mips-ubw32/test-leds.c
@@ -3,23 +3,26 @@
  */
 #include <runtime/lib.h>
 
-#define LEDUSB	8
-#define LED1	4
-#define LED2	2
-#define LED3	1
+#define LEDUSB	8	/* PE3: green */
+#define LED1	4	/* PE2: white */
+#define LED2	2	/* PE1: red */
+#define LED3	1	/* PE0: yellow */
 
 int main (void)
 {
-	PORTE = 0xFFF0;
-	TRISE = 0xFFF0;
+	/* Configure LED pins as output high. */
+	PORTE = LEDUSB | LED1 | LED2 | LED3;
+	TRISE = ! (LEDUSB | LED1 | LED2 | LED3);
+
 	for (;;) {
-		PORTESET = LEDUSB; mdelay (100);
-		PORTESET = LED1;   mdelay (100);
-		PORTESET = LED2;   mdelay (100);
-		PORTESET = LED3;   mdelay (100);
+		/* Continuously turn all LEDS on and off. */
 		PORTECLR = LEDUSB; mdelay (100);
 		PORTECLR = LED1;   mdelay (100);
 		PORTECLR = LED2;   mdelay (100);
 		PORTECLR = LED3;   mdelay (100);
+		PORTESET = LEDUSB; mdelay (100);
+		PORTESET = LED1;   mdelay (100);
+		PORTESET = LED2;   mdelay (100);
+		PORTESET = LED3;   mdelay (100);
 	}
 }
