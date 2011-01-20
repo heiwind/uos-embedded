d1e3231eef56114c6f275d89c6f70202d9141cd6
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2011-01-20 20:09:53 +0000

    PIC32: работает тест task.

--- examples/mips-ubw32/test-task.c
+++ examples/mips-ubw32/test-task.c
@@ -5,23 +5,13 @@
 #include "kernel/uos.h"
 
 ARRAY (task_space, 0x400);	/* Memory for task stack */
-unsigned count_init;		/* Time when uos_init() started */
-unsigned count_task;		/* Time when task() started */
 
 void task (void *arg)
 {
-	unsigned count0;
-
-	count_task = mips_read_c0_register (C0_COUNT);
-	count0 = *(unsigned*) 0xb8400000;
 	for (;;) {
 		debug_printf ("Hello from `%s'!\n", arg);
 		debug_printf ("Task space %d bytes, free %d bytes.\n",
 			sizeof (task_space), task_stack_avail ((task_t*) task_space));
-		debug_printf ("User initialization started after %u ticks.\n",
-			count_init - count0);
-		debug_printf ("First task started after %u ticks.\n",
-			count_task - count0);
 		debug_puts ("(Press Enter)\n");
 		debug_getchar ();
 	}
@@ -29,7 +19,5 @@ void task (void *arg)
 
 void uos_init (void)
 {
-	count_init = mips_read_c0_register (C0_COUNT);
-
 	task_create (task, "task", "task", 1, task_space, sizeof (task_space));
 }
