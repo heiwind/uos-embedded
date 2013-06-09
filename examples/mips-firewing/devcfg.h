0c129b668de1352d47655860eaf823baac6b01ab
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2013-06-09 05:37:50 +0000

    Firewing: using internal RC oscillator. LED test works fine.

--- examples/mips-firewing/devcfg.h
+++ examples/mips-firewing/devcfg.h
@@ -2,20 +2,19 @@
  * Chip configuration.
  */
 PIC32_DEVCFG (
-    DEVCFG0_DEBUG_DISABLED |    /* ICE debugger disabled */
-    DEVCFG0_JTAGDIS |           /* Disable JTAG */
-    DEVCFG0_ICESEL_PAIR2,       /* Use PGC2/PGD2 */
+    DEVCFG0_DEBUG_DISABLED,     /* ICE debugger disabled */
 
-    DEVCFG1_FNOSC_PRIPLL |      /* Primary oscillator with PLL */
-    DEVCFG1_POSCMOD_HS |        /* HS oscillator */
-    DEVCFG1_OSCIOFNC_OFF |      /* CLKO output disabled */
-    DEVCFG1_FPBDIV_1 |          /* Peripheral bus clock = SYSCLK/1 */
-    DEVCFG1_FCKM_DISABLE |      /* Fail-safe clock monitor disable */
-    DEVCFG1_FCKS_DISABLE,       /* Clock switching disable */
+    DEVCFG1_FNOSC_FRCPLL |      /* Fast RC oscillator with PLL */
+    DEVCFG1_POSCMOD_DISABLE |   /* Primary oscillator disabled */
+    DEVCFG1_FPBDIV_2 |          /* Peripheral bus clock = SYSCLK/2 */
+    DEVCFG1_OSCIOFNC_OFF |      /* CLKO output disable */
+    DEVCFG1_FCKM_DISABLE,       /* Fail-safe clock monitor disable */
 
     DEVCFG2_FPLLIDIV_2 |        /* PLL divider = 1/2 */
     DEVCFG2_FPLLMUL_20 |        /* PLL multiplier = 20x */
+    DEVCFG2_UPLLIDIV_2 |        /* USB PLL divider = 1/2 */
     DEVCFG2_UPLLDIS |           /* disable USB PLL */
     DEVCFG2_FPLLODIV_2,         /* PLL postscaler = 1/2 */
 
-    DEVCFG3_USERID(0xffff));    /* User-defined ID */
+    DEVCFG3_USERID(0xffff) |    /* User-defined ID */
+    DEVCFG3_FSRSSEL_7);         /* Assign irq priority 7 to shadow set */
