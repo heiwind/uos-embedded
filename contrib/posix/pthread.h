b59aba7f61669a7494be3eca04e95cef44edc638
a_lityagin@TCT <a_lityagin@TCT>
2015-11-17 10:32:12 +0300

    +posix:pthreads: - множественные дополнения заглушек - pthread_setname_np, pthread_exit, pthread_set/getschedparam, pthread_setschedprio +      sched:sched_get_priority_max/min +posix-port:TASK_PRIORITY_MAX/MIN - локализация диапазона приоритетов задач

--- contrib/posix/pthread.h
+++ contrib/posix/pthread.h
@@ -8,10 +8,18 @@
 #include <kernel/uos.h>
 #include <kernel/internal.h>
 #include <posix-port.h>
+#include <stdlib.h>
+#include <sched.h>
 #if UOS_USLEEP_STYLE == UOS_USLEEP_STYLE_ETIMER_SLEEP
 #include <timer/etimer_threads.h>
 #include <errno.h>
 #endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 
 typedef task_t* pthread_t;
 #define pthread_self()				task_current
@@ -42,11 +50,26 @@ typedef int pthread_key_t;
 #define PTHREAD_STACK_SIZE			0x4000	/* 16 kilobytes */
 #define PTHREAD_PRIO				0x4000	/* 16 kilobytes */
 
+typedef int pthread_attr_t;
+#define pthread_attr_init(x)
+#define pthread_attr_destroy(x)
+#define pthread_setcanceltype(t, oldt)
+
 #define pthread_create(taskp,attr,func,arg)	{ \
 	int stacksz = PTHREAD_STACK_SIZE;	  \
-	char *stack = calloc (1, stacksz);	  \
-	if (stack) *taskp = task_create ((void(*)(void*)) func, \
-		arg, "task", PTHREAD_PRIO, stack, stacksz); }
+	char *stack = (char *)calloc (1, stacksz);	  \
+	if (stack) \
+	    *taskp = task_create ((void(*)(void*)) &(func), \
+		arg, "task", PTHREAD_PRIO, (array_t*)stack, stacksz); }
+
+/* Set thread name visible in the kernel and its interfaces.  */
+INLINE  
+int pthread_setname_np (pthread_t __target_thread, const char *__name) 
+     __THROW //__nonnull((2))
+{
+    __target_thread->name = __name;
+    return 0;
+}
 
 #define pthread_cancel(t)			{ \
 	if (t->base_prio) task_delete (t, 0); 	  \
@@ -61,11 +84,50 @@ typedef int pthread_key_t;
 	t->base_prio = 0;			  \
 	free (t);				}
 
+/*The registered cleanup handlers are called via exception handling
+so we cannot mark this function with __THROW.*/
+INLINE __attribute__ ((__noreturn__)) 
+void pthread_exit (void *__retval)
+{ task_exit(__retval);}
+
 #define pthread_testcancel(t)			/* empty */
 #define pthread_equal(t1, t2)			((t1) == (t2))
 
-#define sched_yield()				task_yield()
 #define setpriority(a,b,c)			/* empty */
+
+/* Set the scheduling parameters for TARGET_THREAD according to POLICY
+   and *PARAM.  */
+INLINE 
+int pthread_setschedparam (pthread_t __target_thread, int __policy,
+                  const struct sched_param *__param)
+     __THROW //__nonnull((3))
+{
+    task_set_priority(__target_thread, __param->__sched_priority);
+    return 0;
+}
+
+/* Set the scheduling priority for TARGET_THREAD.  */
+INLINE
+int pthread_setschedprio (pthread_t __target_thread, int __prio)
+     __THROW
+{
+    task_set_priority(__target_thread, __prio);
+    return 0;
+}
+
+/* Return in *PARAM the scheduling parameters of *ATTR.  */
+INLINE 
+int pthread_attr_getschedparam (pthread_t __target_thread
+                        , int* __policy
+                        , struct sched_param *__restrict __param)
+ __THROW //__nonnull ((2, 3))
+{
+    __param->__sched_priority = task_priority(__target_thread);
+    *__policy = SCHED_RR;
+    return 0;
+}
+
+
 
 typedef mutex_t*    pthread_cond_t;
 
@@ -100,5 +162,11 @@ int pthread_cond_timedwait(pthread_cond_t* __restrict__ cond
 	mutex_wait (lock);			  \
 	0;					})
 #endif //UOS_USLEEP_STYLE
+
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- contrib/posix/sched.h
+++ contrib/posix/sched.h
@@ -1,6 +1,16 @@
 #ifndef __SCHED_H__
 #define __SCHED_H__
 
+#include <kernel/uos.h>
+#include <runtime/sys/uosc.h>
+#include <posix-port.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
 /* Definitions of constants and data structure for POSIX 1003.1b-1993
    scheduling interface.
 
@@ -18,5 +28,38 @@
 # define SCHED_RESET_ON_FORK    0x40000000
 //#endif //__USE_GNU
 
+struct sched_param
+  {
+    int __sched_priority;
+  };
+
+/* Get maximum priority value for a scheduler.  */
+INLINE __CONST
+int sched_get_priority_max (int __algorithm) __THROW
+{
+#ifdef TASK_PRIORITY_MAX
+    return TASK_PRIORITY_MAX;
+#else
+    return 100;
+#endif
+}
+
+/* Get minimum priority value for a scheduler.  */
+INLINE __CONST 
+int sched_get_priority_min (int __algorithm) __THROW
+{
+#ifdef TASK_PRIORITY_MIN
+    return TASK_PRIORITY_MIN;
+#else
+    return 0;
+#endif
+}
+
+#define sched_yield()               task_yield()
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //SCHED
